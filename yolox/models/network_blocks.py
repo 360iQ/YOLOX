8226979c530e9dda5732adf8fd8574888b63ca07
pmaciolek <pmaciolek@dtiq.com>
2025-05-22 13:21:04 +0200

    DSVA-15904 - ViT improvement

--- yolox/models/network_blocks.py
+++ yolox/models/network_blocks.py
@@ -260,26 +260,31 @@ class C3VIT(C3):
         c_ = int(c2 * e)
         self.m = TransformerBlock(c_, c_, 4, n)
 
+
 class TransformerBlock(nn.Module):
-    # Vision Transformer https://arxiv.org/abs/2010.11929
     def __init__(self, c1, c2, num_heads, num_layers):
         super().__init__()
         self.conv1 = None
         if c1 != c2:
             self.conv1 = Conv(c1, c2)
-        self.linear = nn.Linear(c2, c2)
+        self.linear = nn.Linear(c2, c2)  # This serves as positional encoding, but could be improved
         self.tr = nn.Sequential(*(TransformerLayer(c2, num_heads) for _ in range(num_layers)))
         self.c2 = c2
+        # Add learnable positional embeddings
+        self.pos_embed = nn.Parameter(torch.zeros(1, c2, 1, 1))
+        nn.init.trunc_normal_(self.pos_embed, std=0.02)
 
     def forward(self, x):
         if self.conv1 is not None:
             x = self.conv1(x)
+        # Add positional embedding
+        x = x + self.pos_embed.expand(-1, -1, x.size(2), x.size(3))
         b, _, w, h = x.shape
         p = x.flatten(2).permute(2, 0, 1)
         return self.tr(p + self.linear(p)).permute(1, 2, 0).reshape(b, self.c2, w, h)
 
+
 class TransformerLayer(nn.Module):
-    # Transformer layer https://arxiv.org/abs/2010.11929 (LayerNorm layers removed for better performance)
     def __init__(self, c, num_heads):
         super().__init__()
         self.q = nn.Linear(c, c, bias=False)
@@ -288,8 +293,12 @@ class TransformerLayer(nn.Module):
         self.ma = nn.MultiheadAttention(embed_dim=c, num_heads=num_heads)
         self.fc1 = nn.Linear(c, c, bias=False)
         self.fc2 = nn.Linear(c, c, bias=False)
+        # Add layer normalization for stability
+        self.norm1 = nn.LayerNorm(c)
+        self.norm2 = nn.LayerNorm(c)
+        self.act = nn.GELU()  # Commonly used in transformers
 
     def forward(self, x):
-        x = self.ma(self.q(x), self.k(x), self.v(x))[0] + x
-        x = self.fc2(self.fc1(x)) + x
+        x = x + self.ma(self.q(self.norm1(x)), self.k(self.norm1(x)), self.v(self.norm1(x)))[0]
+        x = x + self.fc2(self.act(self.fc1(self.norm2(x))))
         return x
