1c4264f30992ff0447577e7ddda9e7ed78038ac1
Piotr Kolebski <pkolebski@dtiq.com>
2026-07-23 09:54:39 +0200

    refactor: add type annotations to yolox_base.py

--- yolox/exp/yolox_base.py
+++ yolox/exp/yolox_base.py
@@ -2,6 +2,8 @@
 # Copyright (c) Megvii Inc. All rights reserved.
 
 import os
+from typing import Literal
+
 from loguru import logger
 import random
 
@@ -21,25 +23,25 @@ class Exp(BaseExp):
 
         # ---------------- model config ---------------- #
         # detect classes number of model
-        self.num_classes = 80
+        self.num_classes: int = 80
         # factor of model depth
-        self.depth = 1.00
+        self.depth: float = 1.00
         # factor of model width
-        self.width = 1.00
+        self.width: float = 1.00
         # activation name. For example, if using "relu", then "silu" will be replaced to "relu".
-        self.act = "silu"
+        self.act: Literal["silu", "relu"] = "silu"
         # IoU loss type. One of "iou" (standard intersection over union), "giou" (generalized-iou),
         # "diou" (distance-iou) or "ciou" (complete-iou). Based on: https://arxiv.org/pdf/1911.08287.pdf
-        self.iou_loss_type = "iou"
+        self.iou_loss_type: Literal["iou", "silu", "diou", "ciou", "giou"] = "iou"
 
         # ---------------- dataloader config ---------------- #
         # set worker to 4 for shorter dataloader init time
         # If your training process cost many memory, reduce this value.
-        self.data_num_workers = 4
-        self.input_size = (640, 640)  # (height, width)
+        self.data_num_workers: int = 4
+        self.input_size: tuple[int, int] = (640, 640)  # (height, width)
         # Actual multiscale ranges: [640 - 5 * 32, 640 + 5 * 32].
         # To disable multiscale training, set the value to 0.
-        self.multiscale_range = 5
+        self.multiscale_range: int = 5
         # You can uncomment this line to specify a multiscale range
         # self.random_size = (14, 26)
         # dir of dataset images, if data_dir is None, this project will use `datasets` dir
@@ -53,69 +55,69 @@ class Exp(BaseExp):
 
         # --------------- transform config ----------------- #
         # prob of applying mosaic aug
-        self.mosaic_prob = 1.0
+        self.mosaic_prob: float = 1.0
         # prob of applying mixup aug
-        self.mixup_prob = 1.0
+        self.mixup_prob: float = 1.0
         # prob of applying hsv aug
-        self.hsv_prob = 1.0
+        self.hsv_prob: float = 1.0
         # prob of applying flip aug
-        self.flip_prob = 0.5
+        self.flip_prob: float = 0.5
         # rotation angle range, for example, if set to 2, the true range is (-2, 2)
-        self.degrees = 10.0
+        self.degrees: float = 10.0
         # translate range, for example, if set to 0.1, the true range is (-0.1, 0.1)
-        self.translate = 0.1
-        self.mosaic_scale = (0.1, 2)
+        self.translate: float = 0.1
+        self.mosaic_scale: tuple[float, float] = (0.1, 2)
         # apply mixup aug or not
-        self.enable_mixup = True
-        self.mixup_scale = (0.5, 1.5)
+        self.enable_mixup: bool = True
+        self.mixup_scale: tuple[float, float] = (0.5, 1.5)
         # shear angle range, for example, if set to 2, the true range is (-2, 2)
-        self.shear = 2.0
+        self.shear: float = 2.0
 
         # --------------  training config --------------------- #
         # epoch number used for warmup
-        self.warmup_epochs = 5
+        self.warmup_epochs: int = 5
         # max training epoch
-        self.max_epoch = 300
+        self.max_epoch: int = 300
         # minimum learning rate during warmup
-        self.warmup_lr = 0
-        self.min_lr_ratio = 0.05
+        self.warmup_lr: float = 0
+        self.min_lr_ratio: float = 0.05
         # learning rate for one image. During training, lr will multiply batchsize.
-        self.basic_lr_per_img = 0.01 / 64.0
+        self.basic_lr_per_img: float = 0.01 / 64.0
         # name of LRScheduler
         self.scheduler = "yoloxwarmcos"
         # last #epoch to close augmention like mosaic
-        self.no_aug_epochs = 15
+        self.no_aug_epochs: int = 15
         # apply EMA during training
-        self.ema = True
+        self.ema: bool = True
 
         # weight decay of optimizer
-        self.weight_decay = 5e-4
+        self.weight_decay: float = 5e-4
         # momentum of optimizer
-        self.momentum = 0.9
+        self.momentum: float = 0.9
         # log period in iter, for example,
         # if set to 1, user could see log every iteration.
-        self.print_interval = 10
+        self.print_interval: int = 10
         # eval period in epoch, for example,
         # if set to 1, model will be evaluate after every epoch.
-        self.eval_interval = 10
+        self.eval_interval: int = 10
         # save history checkpoint or not.
         # If set to False, yolox will only save latest and best ckpt.
-        self.save_history_ckpt = True
+        self.save_history_ckpt: bool = True
         # name of experiment
-        self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
+        self.exp_name: str = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
 
         # -----------------  testing config ------------------ #
         # output image size during evaluation/test
-        self.test_size = (640, 640)
+        self.test_size: tuple[int, int] = (640, 640)
         # confidence threshold during evaluation/test,
         # boxes whose scores are less than test_conf will be filtered
-        self.test_conf = 0.01
+        self.test_conf: float = 0.01
         # nms threshold
-        self.nmsthre = 0.65
+        self.nmsthre: float = 0.65
         # freeze model backbone weights
-        self.freeze_backbone = False
+        self.freeze_backbone: bool = False
         self.single_image_transforms = None
-        self.class_agnostic_nms_valid = False
+        self.class_agnostic_nms_valid: bool = False
 
     def get_model(self):
         from yolox.models import YOLOX, YOLOPAFPN, YOLOXHead
